15f49751c53183463e0d875dd2e9820cca628a0e
WxJt3k <mateogiraldo220749@correo.itm.edu.co>
2023-02-18 12:17:54 -0500

    Primeros componentes Blazor

---
+++ Sales.WEB/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using Sales.WEB.Repositories.Web.Repositories;
+
+namespace Sales.WEB.Repositories
+{
+    public interface IRepository
+    {
+        Task<HttpResponseWrapper<T>> Get<T>(string url);
+
+        Task<HttpResponseWrapper<Object>> Post<T>(String url, T model);
+
+        Task<HttpResponseWrapper<TResponse>> Post<T,TResponse>(string url, T model);
+    }
+}
