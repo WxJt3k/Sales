f9c3f341bc97093b62373a6c5f7e207d76d42529
WxJt3k <mateogiraldo220749@correo.itm.edu.co>
2023-02-25 21:41:52 -0500

    Revert "Revert "Finalización construccion Countries""
    This reverts commit ec5d965b57495dbb82768a404b3ebaef4e4bf034.

--- Sales.API/Program.cs
+++ Sales.API/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using sale.API.Data;
 using Sales.API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,9 +11,21 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("Name = DB"));
+builder.Services.AddTransient<SeedDb>();
 
 var app = builder.Build();
+SeedData(app);
 
+void SeedData (WebApplication App)
+{
+    IServiceScopeFactory? scopedFactory = app.Services.GetService<IServiceScopeFactory?>();
+
+    using (IServiceScope? scope = scopedFactory!.CreateScope())
+    {
+        SeedDb? service = scope.ServiceProvider.GetService<SeedDb?>();
+        service!.SeedAsync().Wait();
+    }
+}
 
 if (app.Environment.IsDevelopment())
 {
