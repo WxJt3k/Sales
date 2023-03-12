f8dcbb56fd0d1f6683f7ab93298d9c568d5383bd
WxJt3k <mateogiraldo220749@correo.itm.edu.co>
2023-03-11 23:42:42 -0500

    Filters

--- Sales.API/Controllers/StatesController.cs
+++ Sales.API/Controllers/StatesController.cs
@@ -22,16 +22,24 @@ namespace Sales.API.Controllers
         {
             var queryable = _context.States.Include(x => x.Cities).Where(x => x.Country!.Id == pagination.Id).AsQueryable();
 
-            return Ok(await queryable
-                .OrderBy(x => x.Name)
-                .Paginate(pagination)
-                .ToListAsync());
+            if (!string.IsNullOrWhiteSpace(pagination.Filter))
+            {
+                queryable = queryable.Where(x => x.Name.ToLower().Contains(pagination.Filter.ToLower()));
+            }
+
+
+            return Ok(await queryable.OrderBy(x => x.Name).Paginate(pagination).ToListAsync());
         }
 
         [HttpGet("totalPages")]
         public async Task<ActionResult> GetPages([FromQuery] PaginationDTO pagination)
         {
             var queryable = _context.States.Where(x => x.Country!.Id == pagination.Id).AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(pagination.Filter))
+            {
+                queryable = queryable.Where(x => x.Name.ToLower().Contains(pagination.Filter.ToLower()));
+            }
 
             double count = await queryable.CountAsync();
             double totalPages = Math.Ceiling(count / pagination.RecordsNumber);
